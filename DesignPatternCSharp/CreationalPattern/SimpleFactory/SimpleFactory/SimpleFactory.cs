40952a1344e660087b8e22a266a00998508936be
lionel Shen <successwolf@163.com>
2017-04-09 11:56:13 +0800

    C# SimpleFactory
    C# SimpleFactory

---
+++ DesignPatternCSharp/CreationalPattern/SimpleFactory/SimpleFactory/SimpleFactory.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SimpleFactory
+{
+    #region test
+    public interface IProduct { }
+    public class ConcreteProductA : IProduct { }
+    public class ConcreteProductB : IProduct { }
+    public class Factory
+    {
+        public IProduct Create()
+        {
+            return new ConcreteProductA();
+        }
+    }
+    #endregion
+
+    public enum Category
+    {
+        A,
+        B
+    }
+    public static class ProductFactory
+    {
+        public static IProduct Create(Category category)
+        {
+            switch (category)
+            {
+                case Category.A:
+                    return new ConcreteProductA();
+                case Category.B:
+                    return new ConcreteProductB();
+                default:
+                    throw new NotSupportedException();
+            }
+        }
+    }
+
+    class SimpleFactory
+    {
+        static void Main(string[] args)
+        {
+            Factory factoryObj = new Factory();
+            IProduct product = factoryObj.Create();
+        }
+    }
+}
---
+++ DesignPatternCSharp/CreationalPattern/SimpleFactory/SimpleFactory/Test.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace SimpleFactory
+{
+    [TestFixture]
+    public class Test
+    {
+        [Test]
+        public void test()
+        {
+            Factory factoryObj = new Factory();
+            IProduct product = factoryObj.Create();
+            Assert.AreEqual(product.GetType(), typeof(ConcreteProductA));
+        }
+        [Test]
+        public void test1()
+        {
+            IProduct product = ProductFactory.Create(Category.B);
+            Assert.IsNotNull(product);
+            Assert.AreEqual(typeof(ConcreteProductB), product.GetType());
+        }
+    }
+}
